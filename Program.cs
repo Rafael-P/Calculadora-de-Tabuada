60dec4d4645a4649a842466f8f4912ce72a122e8
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-17 13:57:55 -0300

    Algoritmo de tabuada adicionado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Algoritimo__de_Tabuada_17_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero,resultado;
+
+            Console.WriteLine("Digite o número que deseja para apresentar a tabuada");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = numero*i;
+                Console.WriteLine(resultado);
+            }
+
+        }//FIM DO MAIN
+    }
+}
